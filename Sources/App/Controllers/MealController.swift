ae5dc7c932edb2dde9bb2bfd1f22f78a9b42443a
Swain Molster <swain.molster@gmail.com>
2018-08-10 23:58:52 -0400

    Compilation fix

--- Sources/App/Controllers/MealController.swift
+++ Sources/App/Controllers/MealController.swift
@@ -5,4 +5,32 @@
 //  Created by Swain Molster on 8/10/18.
 //
 
-import Foundation
+import Vapor
+import FluentSQLite
+
+final class MealController {
+    
+    /// Returns a list of all meals for the auth'd user.
+    func index(_ req: Request) throws -> Future<[Todo]> {
+        // fetch auth'd user
+        let user = try req.requireAuthenticated(User.self)
+        
+        // query all todo's belonging to user
+        return try  Todo.query(on: req)
+            .filter(\.userID == user.requireID()).all()
+    }
+    
+    /// Creates a new todo for the auth'd user.
+    func create(_ req: Request) throws -> Future<Todo> {
+        // fetch auth'd user
+        let user = try req.requireAuthenticated(User.self)
+        
+        // decode request content
+        return try req.content.decode(CreateTodoRequest.self).flatMap { todo in
+            // save new todo
+            return try Todo(title: todo.title, userID: user.requireID())
+                .save(on: req)
+        }
+    }
+    
+}
--- Sources/App/Models/Meal.swift
+++ Sources/App/Models/Meal.swift
@@ -5,4 +5,19 @@
 //  Created by Swain Molster on 8/10/18.
 //
 
-import Foundation
+import Vapor
+import FluentSQLite
+
+final class Meal: SQLiteModel {
+    var id: Int?
+    
+    var entryDate: Date
+    
+    var userId: User.ID
+    
+    init(id: Int? = nil, entryDate: Date, userId: User.ID) {
+        self.id = id
+        self.entryDate = entryDate
+        self.userId = userId
+    }
+}
